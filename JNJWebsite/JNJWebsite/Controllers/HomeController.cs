390854aa1f8f1443b4ea447b64f9bdd4e5b15375
Jason Few <omence11@gmail.com>
2022-06-11 13:50:19 -0500

    Initial push

---
+++ JNJWebsite/JNJWebsite/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using JNJWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace JNJWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ApiPage()
+        {
+            return View();
+        }
+
+        public IActionResult PublicFacingPage()
+        {
+            return View();
+        }
+
+        public IActionResult WorkFlowPage()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
